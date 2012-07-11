09ac85b2ce5915358e36b89c45ebccf41256cfc2
Michael Moll <kvedulv@kvedulv.de>
2012-07-11 14:59:59 +0200

    initial version

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ puppet-dataprotector
 ====================
 
 Puppet module to manage clients for HP Data Protector
+-----------------------------------------------------
+
+Depends on [puppetlabs-xinetd](https://github.com/puppetlabs/puppetlabs-xinetd)
+
+Example:
+>  class { 'dataprotector':
+>    cm_ip   => '1.2.3.4',
+>    cm_fqdn => 'cellmanager..dom.ain'
+>  }
+
---
+++ manifests/init.pp
@@ -0,0 +1,39 @@
+class dataprotector ($cm_ip, $cm_fqdn) {
+
+  service { 'xinetd':
+    ensure  => 'running'
+    enabled => true
+  }
+
+  xinetd::service { 'omni':
+    port        => '5555',
+    server      => '/opt/omni/lbin/inet',
+    server_args => 'inet -log /var/opt/omni/log/inet.log',
+    flags       => 'IPv4'
+  }
+
+  package { '[ob2-core, ob2-da]':
+    ensure  => 'installed'
+  }
+
+  file { '/var/log/omni':
+    ensure => link,
+    target => '/var/opt/omni/log'
+  }
+
+  host { $cm_fqdn:
+    ensure       => present,
+    ip           => $cm_ip,
+    host_aliases => inline_template('<%= cm_fqdn.split(".")[0] %>'),
+    target       => '/etc/hosts'
+  }
+
+  file { '/etc/opt/omni/client/allow_hosts':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    content => $cm_ip
+  }
+
+}
