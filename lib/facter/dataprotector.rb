8870443d2588cc52f8224bf48e65a4e25cf05b72
Michael Moll <kvedulv@kvedulv.de>
2013-06-18 13:44:41 +0200

    generate some custom facts
    inspired by https://github.com/karmab/puppet/tree/master/dataprotector

---
+++ lib/facter/dataprotector.rb
@@ -0,0 +1,29 @@
+Facter.add(:dataprotector) do
+  setcode do
+    if File.exist? '/etc/opt/omni/client/omni_info'
+      true
+    else
+      false
+    end
+  end
+end
+
+if Facter.value('dataprotector') == true
+  File.readlines('/etc/opt/omni/client/omni_info').each do |line|
+    core_line = /^\-key\ core\ (.*)\-version\ /.match(line)
+    if core_line
+      Facter.add(:dataprotector_version) do
+        setcode do
+          core_line.post_match
+        end
+      end
+    end
+  end
+end 
+
+Facter.add(:dataprotector_patch) do
+  confine :dataprotector => true
+  setcode do
+    File.read('/opt/omni/.patch_core')
+  end
+end
