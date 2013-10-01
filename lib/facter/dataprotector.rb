a96dc12fff2fef9a31181b326426172dbe354f1d
Jean-Denis Gebhardt <jean-denis.gebhardt@noris.net>
2013-10-01 15:53:39 +0200

    shortend code

--- lib/facter/dataprotector.rb
+++ lib/facter/dataprotector.rb
@@ -1,30 +1,16 @@
 Facter.add(:dataprotector) do
-  setcode do
-    if File.exist? '/etc/opt/omni/client/omni_info'
-      true
-    else
-      false
-    end
-  end
+  setcode { File.exist?('/etc/opt/omni/client/omni_info') ? true : false }
 end
 
-if Facter.value('dataprotector') == true
+if Facter.value('dataprotector')
   File.readlines('/etc/opt/omni/client/omni_info').each do |line|
-    core_line = /^\-key\ core\ (.*)\-version\ /.match(line)
-    if core_line
-      Facter.add(:dataprotector_version) do
-        setcode do
-          core_line.post_match.chomp
-        end
-      end
+    Facter.add(:dataprotector_version) do
+      setcode {  (/^\-key\ core\ (.*)\-version\ /.match(line)).post_match.chomp }
     end
+  end
+
+  Facter.add(:dataprotector_patch) do
+    setcode { (File.read('/opt/omni/.patch_core')).chomp }
   end
 end 
 
-Facter.add(:dataprotector_patch) do
-  confine :dataprotector => true
-  setcode do
-    patchlevel = File.read('/opt/omni/.patch_core')
-    patchlevel.chomp
-  end
-end
