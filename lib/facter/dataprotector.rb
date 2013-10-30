17361557c83042d7e9db231dff1fcbf0f6ca71dd
Michael Moll <kvedulv@kvedulv.de>
2013-10-30 21:14:08 +0100

    whitespace fixes

--- lib/facter/dataprotector.rb
+++ lib/facter/dataprotector.rb
@@ -6,12 +6,11 @@ if Facter.value('dataprotector')
   File.readlines('/etc/opt/omni/client/omni_info').each do |line|
     Facter.add(:dataprotector_version) do
       version = (/^\-key\ core\ (.*)\-version\ /.match(line)).post_match.chomp
-      setcode {  version } unless version.empty?
+      setcode { version } unless version.empty?
     end
   end
 
   Facter.add(:dataprotector_patch) do
     setcode { (File.read('/opt/omni/.patch_core')).chomp }
   end
-end 
-
+end
